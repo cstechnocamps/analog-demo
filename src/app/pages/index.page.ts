7f575b725fcb82d462e0556c9bdcc76ac814f869
cstechnocamps <carlo.sarli@technocamps.com>
2024-03-16 13:32:53 +0000

    changed home page docs

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -25,8 +25,8 @@ export const routeMeta: RouteMeta = {
         <h1 class="text-4xl font-bold text-white sm:text-6xl">Create your own blog with Technocamps</h1>
         <p class="mt-6 text-lg leading-8 text-gray-300">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
         <div class="mt-10 flex items-center justify-center gap-x-6">
-          <a href="#" class="rounded-md bg-secondary-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-secondary-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary-400">Get started</a>
-          <a href="#" class="text-sm font-semibold leading-6 text-white">Read the Docs <span aria-hidden="true">→</span></a>
+          <a href="/docs" class="rounded-md bg-secondary-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-secondary-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary-400">Get started</a>
+          <a href="/docs" class="text-sm font-semibold leading-6 text-white">Read the Docs <span aria-hidden="true">→</span></a>
         </div>
       </div>
     </div>
